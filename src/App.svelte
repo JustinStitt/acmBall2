7e86ace171ca47263a5a42b2a925ed0cd5725c48
Justin Stitt <cpt.crixus@gmail.com>
2022-10-25 02:23:37 -0700

    layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,57 @@
 <script>
-  import Game from './lib/Game.svelte';
+	import Game from './lib/Game.svelte';
 </script>
 
+<div class="titlebar">acmBall2 : Electric Boogaloo</div>
 <div class="container">
-  <h1>Hello World</h1>
-  <button>Test</button>
-  <Game />
+	<Game />
+	<div class="editor">Here's Some Code</div>
 </div>
 
 <style>
+	.titlebar {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: calc(12px + 3vw);
+		text-align: center;
+		width: 97vw;
+		height: 10vh;
+		background-color: rgba(27, 26, 26, 0.799);
+		margin-top: 1vh;
+		border: 1px dotted aliceblue;
+	}
+
+	.editor {
+		background-color: green;
+		width: 40vw;
+		height: 80vh;
+	}
+
+	.container {
+		max-width: 97vw;
+		/* max-height: 97vh; */
+
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		padding: 8px;
+
+		background-color: rebeccapurple;
+	}
+
+	@media only screen and (max-width: 490px) {
+		.container {
+			flex-direction: column;
+			/* margin-left: 10vw;
+			margin-right: 10vw; */
+		}
+
+		.editor {
+			width: 97vw;
+			margin-top: 5vh;
+			margin-bottom: 10vh;
+		}
+	}
 </style>
--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -31,32 +31,71 @@
 
 	// run the engine
 	Runner.run(runner, engine);
-
 	onMount(() => {
 		var render = Render.create({
-			element: main_canvas,
+			// element: main_canvas,
+			canvas: main_canvas,
 			engine: engine,
-			options: { height: height, width: width }
+			options: { height: height, width: width, hasBounds: true }
 		});
 
 		Render.run(render);
 	});
 
 	const applyForceToBoxA = () => {
-		Matter.Body.applyForce(boxA, { x: boxA.position.x, y: boxA.position.y }, { x: 0, y: -0.5 });
+		Matter.Body.applyForce(
+			boxA,
+			{ x: boxA.position.x, y: boxA.position.y },
+			{
+				x: 0,
+				y: -0.5
+			}
+		);
 	};
+
+	const changeBoxAPosition = (new_pos) => {
+		Matter.Body.setPosition(boxA, { x: new_pos, y: boxA.position.y });
+	};
+
+	let gamma = 0;
+	setInterval(() => {
+		gamma += 0.11;
+		Matter.Body.setPosition(boxA, { x: Math.sin(gamma) * 50 + x_slider, y: boxA.position.y });
+	}, 1);
+
+	$: changeBoxAPosition(x_slider);
+
+	let x_slider = 200;
 </script>
 
-<div class="game-container" bind:this={main_canvas}>
-	<!-- <P5 {sketch} /> -->
+<div class="game-container">
+	<canvas id="main-canvas" bind:this={main_canvas} />
 </div>
 
-<button on:click={applyForceToBoxA}>Apply Force to boxA</button>
-
+<!-- <button on:click={applyForceToBoxA}>Apply Force to boxA</button>
+<input type="range" min="0" max="300" bind:value={x_slider} /> -->
 <style>
+	#main-canvas,
 	.game-container {
-		padding: 8px;
-		margin: 8px;
+		border-radius: 2%;
+	}
+
+	#main-canvas {
+		width: 100%;
+		height: 100%;
+	}
+
+	.game-container {
+		width: 35vw;
+		/* min-width: 400px; */
+		aspect-ratio: 1;
 		background-color: goldenrod;
+		padding: 8px;
+	}
+
+	@media only screen and (max-width: 490px) {
+		.game-container {
+			width: 78vw;
+		}
 	}
 </style>
