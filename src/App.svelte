7984a0ec8c46dc060cd0d0990310750d6dd9df67
Justin Stitt <cpt.crixus@gmail.com>
2022-11-06 03:42:27 -0800

    add swap view toggle

--- src/App.svelte
+++ src/App.svelte
@@ -6,15 +6,34 @@
 	onMount(() => {});
 
 	let game;
+
+	let left_view = true;
 </script>
 
-<div class="titlebar">acmBall2 : Electric Boogaloo</div>
-<div class="container">
+<div class="titlebar">
+	<button
+		class="swap-view-toggle"
+		on:click={() => {
+			left_view = !left_view;
+		}}
+		>Swap View
+	</button>
+	<h5>acmBall2 : Electric Boogaloo</h5>
+</div>
+
+<div class="container" style={`flex-direction: ${!left_view ? 'row-reverse' : 'row'}`}>
 	<Game bind:this={game} />
 	<Editor on:compileCode={() => game.compileCode()} on:stopCode={() => game.stopCode()} />
 </div>
 
 <style>
+	.titlebar > h5 {
+		margin-right: auto;
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+
 	.titlebar {
 		display: flex;
 		justify-content: center;
@@ -26,11 +45,11 @@
 		background-color: rgba(27, 26, 26, 0.799);
 		margin-top: 1vh;
 		border: 1px dotted aliceblue;
+		padding: 0.1em;
 	}
 
 	.container {
 		max-width: 97vw;
-
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
@@ -39,6 +58,19 @@
 
 		background-color: rgba(47, 39, 56, 0.293);
 		border: 2px inset aliceblue;
+	}
+
+	.swap-view-toggle {
+		font-size: 0.5em;
+		width: 10%;
+		height: 100%;
+		background-color: #242424;
+		border: 1px solid aliceblue;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-right: auto;
+		padding: 0.05em;
 	}
 
 	@media only screen and (max-width: 490px) {
